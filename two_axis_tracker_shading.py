6c91c3d435b26583193d8553c4c0e12e4dd64b46
AdamRJensen <adam-r-j@hotmail.com>
2021-09-01 10:35:41 +0200

    Update two_axis_tracker_shading.py

--- two_axis_tracker_shading.py
+++ two_axis_tracker_shading.py
@@ -11,12 +11,11 @@ from matplotlib.patches import Polygon
 import shapely
 
 
-def rotate_origin(x, y, rotation_rad):
-    """Rotate a set of 2D points the origin (0, 0).
-    https://gist.github.com/LyleScott/e36e08bfb23b1f87af68c9051f985302
+def _rotate_origin(x, y, rotation_deg):
+    """Rotate a set of 2D points counterclockwise around the origin (0, 0).
     """
-    # rotation_rad = np.deg2rad(rotation_deg)
-    # Rotation is set negative to make counter-clockwise rotation
+    rotation_rad = np.deg2rad(rotation_deg)
+    # Rotation is set negative to make counterclockwise rotation
     xx = x * np.cos(-rotation_rad) + y * np.sin(-rotation_rad)
     yy = -x * np.sin(-rotation_rad) + y * np.cos(-rotation_rad)
     return xx, yy
@@ -52,12 +51,18 @@ def generate_field_layout(gcr, collector_area, L_min, neighbor_order,
                           aspect_ratio=None, offset=None, rotation=None,
                           layout_type=None, plot=True):
     """
-    Generate regular-spaced collector field layout.
+    Generate a regular-spaced collector field layout.
+
+
+    The field layout can be specified either using selecting a standard layout
+    using the layout_type argument or by specifying the individual layout
+    parameters aspect_ratio, offset, and rotation. For both cases also the
+    ground cover ratio needs to be specified.
 
     Parameters
     ----------
     gcr: float
-        Ground coverage ratio. Ratio of collector area to ground area.
+        Ground cover ratio. Ratio of collector area to ground area.
     collector_area: float
         Surface area of one collector.
     L_min: float
@@ -66,55 +71,66 @@ def generate_field_layout(gcr, collector_area, L_min, neighbor_order,
     neighbor_order: int
         Order of neighbors to include in layout. neighbor_order=1 includes only
         the 8 directly adjacent collectors.
-    aspect_ratio: float, default: None
+    aspect_ratio: float, optional
         Ratio of the spacing in the primary direction to the secondary.
-    offset: float, default: None
+    offset: float, optional
         Relative row offset in the secondary direction as fraction of the
         spacing in the secondary direction. -0.5 <= offset < 0.5.
-    rotation: float, default: None
-        Counter-clockwise rotation of the array in radians. 0 <= rotation < pi
-    layout_type: {square, square_rotated, hexagon_e_w, hexagon_n_s}, default: None
+    rotation: float, optional
+        Counterclockwise rotation of the field in degrees. 0 <= rotation < 180
+    layout_type: {square, square_rotated, hexagon_e_w, hexagon_n_s}, optional
         Specification of the special layout type (only depend on gcr).
     plot: bool, default: True
         Whether to plot the field layout.
 
     Returns
     -------
-    X
-    Y
-    tracker_distance
+    X: array of floats
+        x coordinates of neighboring trackers.
+    Y: array of floats
+        y coordinates of neighboring trackers.
+    tracker_distance: array of floats
+        Distances between neighboring trackers and reference tracker.
     relative_azimuth: array of floats
+        Relative azimuth between neigboring trackers and reference tracker.
 
     Notes
     -----
-    Any units for distance can be used as long as the usage is consistent with
-    the collector geometry.
+    Any length unit can be used as long as the usage is consistent with the
+    collector geometry.
 
+    References
+    ----------
+    .. [1] `GitHub repository for this code
+       <https://github.com/AdamRJensen/two_axis_tracker_shading/>`_
     """  # noqa: E501
     # Consider special layouts which can be defined only by GCR
     if layout_type == 'square':
         aspect_ratio = 1
         offset = 0
         rotation = 0
-    elif layout_type == 'square_rotated':
+    # Diagonal layout is the square layout rotated 45 degrees
+    elif layout_type == 'diagonal':
         aspect_ratio = 1
         offset = 0
-        rotation = np.pi / 4  # 45 degree rotation
+        rotation = 45
+    # Hexagonal layouts are defined by an aspect ratio=0.866 and offset=-0.5
     elif layout_type == 'hexagonal_n_s':
         aspect_ratio = np.sqrt(3)/2
         offset = -0.5
         rotation = 0
+    # The hexagonal E-W layout is the hexagonal N-S layout rotated 90 degrees
     elif layout_type == 'hexagonal_e_w':
         aspect_ratio = np.sqrt(3)/2
         offset = -0.5
-        rotation = np.pi/2  # 90 degree rotation of hexagonal_e_w
+        rotation = 90
     elif layout_type is not None:
         raise ValueError('The layout type specified was not recognized.')
     elif ((aspect_ratio is None) or (offset is None) or (rotation is None)):
         raise ValueError('Aspect ratio, offset, and rotation needs to be '
                          'specified when no layout type has not been selected')
 
-    # Check parameters are within their ranges for non-special layouts
+    # Check parameters are within their ranges
     if aspect_ratio < np.sqrt(1-offset**2):
         raise ValueError('Aspect ratio is too low and not feasible')
     if aspect_ratio > collector_area/(gcr*L_min**2):
@@ -136,7 +152,7 @@ def generate_field_layout(gcr, collector_area, L_min, neighbor_order,
     # Generation of X and Y arrays with coordinates
     X = np.tile(np.arange(int(-N/2), int(N/2)+1), N)
     Y = np.repeat(np.arange(int(-N/2), int(N/2)+1), N)
-    # Remove center point (origin)
+    # Remove reference collector point (origin)
     X = np.delete(X, int(N**2/2))
     Y = np.delete(Y, int(N**2/2))
 
@@ -144,13 +160,15 @@ def generate_field_layout(gcr, collector_area, L_min, neighbor_order,
     # calculate offset as it relies on the original X array.
     Y = Y + offset*X
     X = X * aspect_ratio
-    # Rotate and scale array
-    X, Y = rotate_origin(X, Y, rotation)
+    # Apply field rotation
+    X, Y = _rotate_origin(X, Y, rotation)
+    # Calculate and apply the scaling factor based on GCR
     scaling = np.sqrt(collector_area / (gcr * aspect_ratio))
     X, Y = X*scaling, Y*scaling
 
     # Calculate distance and angle of shading trackers relative to the center
     tracker_distance = np.sqrt(X**2 + Y**2)
+    # The relative azimuth is defined clockwise eastwards from north
     relative_azimuth = np.mod(450-np.rad2deg(np.arctan2(Y, X)), 360)
 
     # Visualize layout
@@ -161,9 +179,34 @@ def generate_field_layout(gcr, collector_area, L_min, neighbor_order,
 
 
 def two_axis_shading_fraction(solar_azimuth, solar_elevation,
-                              collector_geometry, tracker_distance,
-                              relative_azimuth, L_min, plot=False):
-    """Calculate shading fraction"""
+                              collector_geometry, L_min, tracker_distance,
+                              relative_azimuth, plot=False):
+    """Calculate the shading fraction for any layout of two-axis tracking collectors.
+
+    Parameters
+    ----------
+    solar_azimuth: float
+        Solar azimuth angle in degrees.
+    solar_elevation: float
+        Solar elevation angle in degrees.
+    collector_geometry: Shapely geometry object
+        The collector aperture geometry.
+    L_min: float
+        Minimum distance between collectors. Used for selecting possible
+        shading collectors.
+    tracker_distance: array of floats
+        Distances between neighboring trackers and reference tracker.
+    relative_azimuth: array of floats
+        Relative azimuth between neigboring trackers and reference tracker.
+    plot: bool, default: True
+        Whether to plot the projected shadows.
+
+    Returns
+    -------
+    shading_fraction: float
+        Shading fraction for the specific solar position and field layout.
+    """  # noqa: E501
+    # If the sun is below the horizon, set the shading fraction to nan
     if solar_elevation < 0:
         return np.nan
 
@@ -172,29 +215,24 @@ def two_axis_shading_fraction(solar_azimuth, solar_elevation,
     # Create mask to only calculate shading for collectors within +/-90° view
     mask = np.where(np.cos(np.deg2rad(azimuth_difference)) > 0)
 
-    # Should apply mask at this step! -> also insert azimuth_difference in
-    # equation
-    xoff = tracker_distance*np.sin(np.deg2rad(solar_azimuth-relative_azimuth))
-    yoff = -tracker_distance\
-        * np.cos(np.deg2rad(solar_azimuth-relative_azimuth))\
+    xoff = tracker_distance[mask]*np.sin(np.deg2rad(azimuth_difference[mask]))
+    yoff = -tracker_distance[mask]\
+        * np.cos(np.deg2rad(azimuth_difference[mask]))\
         * np.sin(np.deg2rad(solar_elevation))
 
-    unshaded_area = collector_geometry
     shade_patches = []
-    for i, (x, y) in enumerate(zip(xoff[mask], yoff[mask])):
+    for i, (x, y) in enumerate(zip(xoff, yoff)):
         if np.sqrt(x**2+y**2) < L_min:
             # Project the geometry of the shading collector onto the plane
             # of the investigated collector
-            shade_geometry = shapely.affinity.translate(collector_geometry,
-                                                        x, y)
+            shade_geometry = shapely.affinity.translate(collector_geometry, x, y)  # noqa: E501
             # Determine the unshaded area
-            unshaded_area = unshaded_area.difference(shade_geometry)
+            unshaded_area = collector_geometry.difference(shade_geometry)
             if plot:
                 shade_patches.append(Polygon(shade_geometry.exterior))
 
     shading_fraction = 1 - unshaded_area.area / collector_geometry.area
 
-    # make figure
     if plot:
         shade_patches = PatchCollection(shade_patches, facecolor='blue',
                                         linewidth=0.5, alpha=0.5)
