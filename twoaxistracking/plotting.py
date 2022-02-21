e8bf8b803adb827f7487f896a442800c61d16cb5
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2022-02-21 22:41:18 +0100

    from matplotlib import collections

--- twoaxistracking/plotting.py
+++ twoaxistracking/plotting.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib.collections import EllipseCollection
-from matplotlib.collections import PatchCollection
+from matplotlib import collections
 from matplotlib import patches
 from shapely import geometry
 
@@ -9,20 +8,15 @@ def _plot_field_layout(X, Y, L_min):
     """Plot field layout."""
     fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(aspect='equal'))
     # Plot a circle for each neighboring collector (diameter equals L_min)
-    ax.add_collection(EllipseCollection(widths=L_min, heights=L_min,
-                                        angles=0, units='xy',
-                                        facecolors='white',
-                                        edgecolors=("black",),
-                                        linewidths=(1,),
-                                        offsets=list(zip(X, Y)),
-                                        transOffset=ax.transData))
+    ax.add_collection(collections.EllipseCollection(
+        widths=L_min, heights=L_min, angles=0, units='xy', facecolors='white',
+        edgecolors=("black",), linewidths=(1,), offsets=list(zip(X, Y)),
+        transOffset=ax.transData))
     # Add a circle for the origin (reference collector)
-    ax.add_collection(EllipseCollection(widths=L_min, heights=L_min,
-                                        angles=0, units='xy',
-                                        facecolors='red',
-                                        edgecolors=("black",),
-                                        linewidths=(1,), offsets=[0, 0],
-                                        transOffset=ax.transData))
+    ax.add_collection(collections.EllipseCollection(
+        widths=L_min, heights=L_min, angles=0, units='xy', facecolors='red',
+        edgecolors=("black",), linewidths=(1,), offsets=[0, 0],
+        transOffset=ax.transData))
     ax.grid()
     ax.set_ylim(min(Y)-L_min, max(Y)+L_min)
     ax.set_xlim(min(X)-L_min, max(X)+L_min)
@@ -37,7 +31,7 @@ def _polygons_to_patch_collection(geometries, **kwargs):
     if isinstance(geometries, geometry.Polygon):
         geometries = geometry.MultiPolygon([geometries])
     exteriors = [patches.Polygon(g.exterior) for g in geometries]
-    path_collection = PatchCollection(exteriors, **kwargs)
+    path_collection = collections.PatchCollection(exteriors, **kwargs)
     return path_collection
 
 
