9210ac344e0ab0a318061495ddb2b198e778a7ee
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2022-02-28 21:49:08 +0100

    Rework intro_tutorial.ipynb

--- twoaxistracking/plotting.py
+++ twoaxistracking/plotting.py
@@ -7,10 +7,10 @@ from matplotlib import cm
 
 
 def _plot_field_layout(X, Y, Z, min_tracker_spacing):
-    """Plot field layout."""
+    """Create a plot of the field layout."""
     # Collector heights is illustrated with colors from a colormap
     norm = mcolors.Normalize(vmin=min(Z)-0.000001, vmax=max(Z)+0.000001)
-    # 0.000001 is added/subtracted for the limits in order for the colormap
+    # 0.000001 is added/subtracted to/from the limits in order for the colormap
     # to correctly display the middle color when all tracker Z coords are zero
     cmap = cm.viridis_r
     colors = cmap(norm(Z))
@@ -25,14 +25,13 @@ def _plot_field_layout(X, Y, Z, min_tracker_spacing):
         widths=min_tracker_spacing, heights=min_tracker_spacing, angles=0,
         units='xy', facecolors='red', edgecolors=("black",), linewidths=(1,),
         offsets=[0, 0], transOffset=ax.transData))
-    plt.axis('equal')
     fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax, shrink=0.8,
                  label='Relative tracker height (vertical)')
     # Set limits
     lower_lim = min(min(X), min(Y)) - min_tracker_spacing
     upper_lim = max(max(X), max(Y)) + min_tracker_spacing
-    ax.set_ylim(lower_lim, upper_lim)
     ax.set_xlim(lower_lim, upper_lim)
+    ax.set_ylim(lower_lim, upper_lim)
     return fig
 
 
