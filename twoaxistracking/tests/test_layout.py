f435e1ca9a57189a4e6a6f72bb1adc5da943c4fb
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2022-03-02 15:52:15 +0100

    Add tests (#16)
    * Create twoaxistrackerfield.py
    
    * Add TwoAxisTrackerField to __init__.py and documentation.rst
    
    * Add _calculate_l_min to layout
    
    * Fix linter error
    
    * Update twoaxistrackerfield.py
    
    * Reorder layout ValueError warnings
    
    * Fix typo
    
    * Remove layout_type from generate_field_layout function
    
    * Add layout_type code to TwoAxisTrackerField
    
    * L_min -> min_tracker_spacing
    
    * Add whatsnew entry
    
    * Fix linting errors
    
    * Update twoaxistrackerfield.py
    
    * Correct horizon shading
    
    * Update twoaxistracking/twoaxistrackerfield.py
    
    Co-authored-by: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>
    
    * Update twoaxistracking/twoaxistrackerfield.py
    
    Co-authored-by: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>
    
    * Update twoaxistracking/twoaxistrackerfield.py
    
    Co-authored-by: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>
    
    * Return scalar when scalar is passed
    
    * Correct use of np.isscalar
    
    * Update documentation
    
    * Update whatsnew
    
    * Rework intro_tutorial.ipynb
    
    * Update README.md
    
    * Create test_layout.py
    
    * Update test_layout.py
    
    * Use test folder in package folder
    
    * Delete test_placeholder.py
    
    * Add test files
    
    * Fix linting errors
    
    * Update scalar test
    
    * Add pandas to test in setup.cfg
    
    * Revert "package metadata and doc build updates (#17)"
    
    This reverts commit e9b5810f72e0dd62c63c0915e27f910405f009fc.
    
    * Undo commits
    
    * Revert "Undo commits"
    
    This reverts commit 147ba2a5f48d973469441f1cf2c2318acff6cc93.
    
    * Add test files
    
    * Switch to using np.testing.assert_allclose
    
    * Add test for sloped field layout
    
    * Move test_twoaxistracker to twoaxistracker PR
    
    * Add conftest.py to avoid duplicate fixtures
    
    * Remove imports of fixtures from conftest
    
    * Remove unnecessary imports
    
    * Increase test tolerance for test_field_slope
    
    * Add multi-polygon active area test
    
    * Add empty __init__.py
    
    * Use "from .conftest" as Kevin said..
    
    * Base square_field_layout_slope fixture off square_field_layout
    
    * Fix linter error
    
    * Remove backslash in conftest.py
    
    Co-authored-by: Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>

---
+++ twoaxistracking/tests/test_layout.py
@@ -0,0 +1,147 @@
+from twoaxistracking import layout
+from shapely import geometry
+import numpy as np
+import pytest
+
+
+def test_min_tracker_spacing_rectangle(rectangular_geometry):
+    # Test calculation of min_tracker_spacing for a rectangular collector
+    min_tracker_spacing = layout._calculate_min_tracker_spacing(rectangular_geometry[0])
+    np.testing.assert_allclose(min_tracker_spacing, np.sqrt(4**2+2**2))
+
+
+def test_min_tracker_spacing_circle():
+    # Test calculation of min_tracker_spacing for a circular collector with radius 1
+    collector_geometry = geometry.Point(0, 0).buffer(1)
+    min_tracker_spacing = layout._calculate_min_tracker_spacing(collector_geometry)
+    assert min_tracker_spacing == 2
+
+
+def test_min_tracker_spacing_circle_offcenter():
+    # Test calculation of min_tracker_spacing for a circular collector with radius 1 rotating
+    # off-center around the point (0, 1)
+    collector_geometry = geometry.Point(0, 1).buffer(1)
+    min_tracker_spacing = layout._calculate_min_tracker_spacing(collector_geometry)
+    assert min_tracker_spacing == 4
+
+
+def test_min_tracker_spacingpolygon():
+    # Test calculation of min_tracker_spacing for a polygon
+    collector_geometry = geometry.Polygon([(-1, -1), (3, 2), (4, 4), (1, 2), (-1, -1)])
+    min_tracker_spacing = layout._calculate_min_tracker_spacing(collector_geometry)
+    np.testing.assert_allclose(min_tracker_spacing, 2 * np.sqrt(4**2 + 4**2))
+
+
+def test_square_layout_generation(rectangular_geometry, square_field_layout):
+    # Test that a square field layout is returned correctly
+    collector_geometry, total_collector_area, min_tracker_spacing = rectangular_geometry
+    X_exp, Y_exp, Z_exp, tracker_distance_exp, relative_azimuth_exp, relative_slope_exp = \
+        square_field_layout
+
+    X, Y, Z, tracker_distance, relative_azimuth, relative_slope = \
+        layout.generate_field_layout(
+            gcr=0.125,
+            total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing,
+            neighbor_order=1,
+            aspect_ratio=1,
+            offset=0,
+            rotation=0)
+    np.testing.assert_allclose(X, X_exp)
+    np.testing.assert_allclose(Y, Y_exp)
+    np.testing.assert_allclose(Z, Z_exp)
+    np.testing.assert_allclose(tracker_distance_exp, tracker_distance_exp)
+    np.testing.assert_allclose(relative_azimuth, relative_azimuth_exp)
+    np.testing.assert_allclose(relative_slope, relative_slope_exp)
+
+
+def test_field_slope(rectangular_geometry, square_field_layout_sloped):
+    # Test that a square field layout on tilted surface is returned correctly
+    collector_geometry, total_collector_area, min_tracker_spacing = rectangular_geometry
+    X_exp, Y_exp, Z_exp, tracker_distance_exp, relative_azimuth_exp, relative_slope_exp = \
+        square_field_layout_sloped
+    X, Y, Z, tracker_distance, relative_azimuth, relative_slope = \
+        layout.generate_field_layout(
+            gcr=0.125,
+            total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing,
+            neighbor_order=1,
+            aspect_ratio=1,
+            offset=0,
+            rotation=0,
+            slope_azimuth=45,
+            slope_tilt=5)
+    np.testing.assert_allclose(X, X_exp)
+    np.testing.assert_allclose(Y, Y_exp)
+    np.testing.assert_allclose(Z, Z_exp, atol=10**-9)
+    np.testing.assert_allclose(tracker_distance_exp, tracker_distance_exp)
+    np.testing.assert_allclose(relative_azimuth, relative_azimuth_exp)
+    np.testing.assert_allclose(relative_slope, relative_slope_exp, atol=10**-9)
+
+
+def test_layout_generation_value_error(rectangular_geometry):
+    # Test if value errors are correctly raised
+    collector_geometry, total_collector_area, min_tracker_spacing = rectangular_geometry
+
+    # Test if ValueError is raised if offset is out of range
+    with pytest.raises(ValueError, match="offset is outside the valid range"):
+        _ = layout.generate_field_layout(
+            gcr=0.25, total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing, neighbor_order=1,
+            aspect_ratio=1, offset=1.1, rotation=0)
+
+    # Test if ValueError is raised if aspect ratio is too low
+    with pytest.raises(ValueError, match="Aspect ratio is too low"):
+        _ = layout.generate_field_layout(
+            gcr=0.25, total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing, neighbor_order=1,
+            aspect_ratio=0.6, offset=0, rotation=0)
+
+    # Test if ValueError is raised if aspect ratio is too high
+    with pytest.raises(ValueError, match="Aspect ratio is too high"):
+        _ = layout.generate_field_layout(
+            gcr=0.25, total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing, neighbor_order=1,
+            aspect_ratio=5, offset=0, rotation=0)
+
+    # Test if ValueError is raised if rotation is greater than 180 degrees
+    with pytest.raises(ValueError, match="rotation is outside the valid range"):
+        _ = layout.generate_field_layout(
+            gcr=0.25, total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing, neighbor_order=1,
+            aspect_ratio=1.2, offset=0, rotation=190)
+
+    # Test if ValueError is raised if rotation is less than 0
+    with pytest.raises(ValueError, match="rotation is outside the valid range"):
+        _ = layout.generate_field_layout(
+            gcr=0.5, total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing, neighbor_order=1,
+            aspect_ratio=1, offset=0, rotation=-1)
+
+    # Test if ValueError is raised if min_tracker_spacing is outside valid range
+    with pytest.raises(ValueError, match="Lmin is not physically possible"):
+        _ = layout.generate_field_layout(
+            gcr=0.25, total_collector_area=total_collector_area,
+            min_tracker_spacing=1, neighbor_order=1, aspect_ratio=1.2,
+            offset=0, rotation=90)
+
+    # Test if ValueError is raised if maximum ground cover ratio is exceeded
+    with pytest.raises(ValueError, match="Maximum ground cover ratio exceded"):
+        _ = layout.generate_field_layout(
+            gcr=0.5, total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing, neighbor_order=1,
+            aspect_ratio=1, offset=0, rotation=0)
+
+
+def test_neighbor_order(rectangular_geometry):
+    collector_geometry, total_collector_area, min_tracker_spacing = rectangular_geometry
+    X, Y, Z, tracker_distance, relative_azimuth, relative_slope = \
+        layout.generate_field_layout(
+            gcr=0.125,
+            total_collector_area=total_collector_area,
+            min_tracker_spacing=min_tracker_spacing,
+            neighbor_order=3,
+            aspect_ratio=1,
+            offset=0,
+            rotation=0)
+    assert len(X) == (7*7-1)
---
+++ twoaxistracking/tests/test_plotting.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+from twoaxistracking import plotting, twoaxistrackerfield
+from .conftest import assert_isinstance
+import numpy as np
+
+
+def test_field_layout_plot():
+    X = np.array([-8, 0, 8, -8, 8, -8, 0, 8])
+    Y = np.array([-8, -8, -8, 0, 0, 8, 8, 8])
+    Z = np.array([0.1237, 0.0619, 0, 0.0619, -0.0619, 0, -0.0619, -0.1237])
+    L_min = 4.4721
+    result = plotting._plot_field_layout(X, Y, Z, L_min)
+    assert_isinstance(result, plt.Figure)
+    plt.close('all')
+
+
+def test_shading_plot(rectangular_geometry, active_geometry_split):
+    collector_geometry, total_collector_area, min_tracker_spacing = rectangular_geometry
+    result = plotting._plot_shading(
+        active_geometry_split,
+        collector_geometry,
+        collector_geometry,
+        min_tracker_spacing)
+    assert_isinstance(result, plt.Figure)
+    plt.close('all')
+
+
+def test_plotting_of_field_layout(rectangular_geometry):
+    # Test if plot_field_layout returns a figure object
+    collector_geometry, total_collector_area, min_tracker_spacing = rectangular_geometry
+    field = twoaxistrackerfield.TwoAxisTrackerField(
+        total_collector_geometry=collector_geometry,
+        active_collector_geometry=collector_geometry,
+        neighbor_order=1,
+        gcr=0.25,
+        aspect_ratio=1,
+        offset=0.45,
+        rotation=30,
+    )
+    result = field.plot_field_layout()
+    assert_isinstance(result, plt.Figure)
+    plt.close('all')
