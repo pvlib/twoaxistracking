0c7376e9fb7ef5f327a680b1b5b3f6b4215f5561
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2023-01-22 23:09:49 +0100

    Fix twoaxistracking.__version__ (#45)
    * make twoaxistracking.__version__ work
    
    * Hyperlink license in README
    
    * Skip testing of new lines in .__init__
    
    * Update docs/source/whatsnew.md
    
    Co-authored-by: Kevin Anderson <kevin.anderson@nrel.gov>
    
    * Move importlib to install requires
    
    Co-authored-by: Kevin Anderson <kevin.anderson@nrel.gov>

--- twoaxistracking/__init__.py
+++ twoaxistracking/__init__.py
@@ -1,3 +1,15 @@
+try:  # pragma: no cover
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    # for python < 3.8 (remove when dropping 3.7 support)
+    from importlib_metadata import PackageNotFoundError, version
+
+try:  # pragma: no cover
+    __version__ = version(__package__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0+unknown"
+
+
 # Import of functions that should be accessible from the package top-level
 from .layout import generate_field_layout  # noqa: F401
 from .shading import shaded_fraction  # noqa: F401
