fd63d47a2bb908bb0ac4638e7422bdeabcafb9d7
Kevin Anderson <kevin.anderson@nrel.gov>
2024-05-14 16:28:19 -0400

    nix pkg_resources (#49)

--- twoaxistracking/tests/test_twoaxistracking.py
+++ twoaxistracking/tests/test_twoaxistracking.py
@@ -1,4 +1,4 @@
-from pkg_resources import parse_version
+from packaging.version import Version
 import twoaxistracking
 
 
@@ -6,5 +6,5 @@ def test___version__():
     # check that the version string is determined correctly.
     # if the version string is messed up for some reason, it should be
     # '0+unknown', which is not greater than '0.0.1'.
-    version = parse_version(twoaxistracking.__version__)
-    assert version > parse_version('0.0.1')
+    version = Version(twoaxistracking.__version__)
+    assert version > Version('0.0.1')
